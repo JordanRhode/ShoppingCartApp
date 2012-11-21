e140569f01de4cd811d343825d2f57e492ef597f
Jordan R. Rhode <rhode.jordan@gmail.com>
2012-11-21 16:24:07 -0600

    modified specific product info
    I formated the page, added a quantity select, checkout button, and
    cropped the pictures

--- getprod.php
+++ getprod.php
@@ -7,7 +7,7 @@
 				if(isset($_GET["name"]))
 				{
 					$prodName = $_GET['name'];
-					echo "<p>" . $prodName . "</p>";
+					
 					$xml = simplexml_load_file("assets/xml/productList.xml")
 					or die("Unable to lad XML file");
 				
@@ -19,14 +19,41 @@
 							$largeImg = $product->large;
 						}
 					}
-					echo "<br/>" . $description;
-					echo "<br/>$" . $price;
+					
 			?>		
-		</div>
+		
 		<div id="product_image">
         	<?php
 				echo "<img src='" . $largeImg . "' alt='" . $prodName . "' height='400' width='400'/>";
 			?>
+		</div>
+		<div id="product_text">
+			<?php
+				echo "<h2>" . $prodName . "</h2>";
+				echo "<br/>" . $description;
+				echo "<br/>$" . $price;
+			?>
+			<form name="product" action="#" method="post">
+					<?php
+						if($prodName == "Solution Rock Shoes"){
+							echo "Size: <select>";
+							for($size = 5; $size <= 14; $size++){
+								echo "<option value='" . $size . "'>" . $size . "</option>";
+							}
+							echo "</select><br/>";
+						}
+					?>
+				Quantity: 
+				<select>
+					<?php
+						for($q = 0; $q <= $quan; $q++){
+							echo "<option value='" . $q . "'>" . $q . "</option>";
+						}
+					?>
+				</select>
+				<input type="submit" value="Add to cart">
+			</form>
+		</div>
 		</div>
 		<?php } 
 			require_once "footer.php";
