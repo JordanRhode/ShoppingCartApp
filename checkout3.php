98940972d7c1cb8adbccd0630242aaa34ed77a8e
Peter Rose <rosepeter24@gmail.com>
2012-11-26 16:47:13 -0600

    final

--- checkout3.php
+++ checkout3.php
@@ -9,9 +9,10 @@
 		$userID = $_SESSION['userID'];
 		$bAddressID = $_SESSION['baddressID'];
 		$sAddressID = $_SESSION['saddressID'];
+		$subtotal = $_SESSION['subtotal'];
 		$totQuan = 0;
-		$st = $_SESSION['subtotal'];
-		unset($_SESSION['subtotal']);
+		$shipCost = 0;
+		$totalCost = 0;
 		
 		//get user's transactions
 		$sql = "SELECT numTransactions FROM rrtable_user
@@ -87,32 +88,57 @@
 		$sCity = $row['city'];
 		$sState = $row['state'];
 		
-		//INSERT some info into order table
+		//Calculate shipping cost
+		$shipCost = $totQuan * 4.95;
+		
+		//Get Date
+		$orderDate = date ("Y-m-d", time());
+		
+		//Calculate Total
+		$totalCost = $subtotal + $shipCost;
+		
+		//INSERT obtained info into order table
+		$sql = "INSERT INTO rrtable_order (transID, orderDate, subtotal, shipCost, total)
+				VALUES ('$transID', '$orderDate', '$subtotal', '$shipCost', '$totalCost')";
+		mysql_query($sql, $conn) or die('Order Input error: ' . mysql_error());	
+			
 		//Get order id
+		$sql = "SELECT orderID FROM rrtable_order
+				WHERE transID = '$transID' AND
+					orderDate = '$orderDate'";
+		$result = mysql_query($sql, $conn) or die('Order ID error: ' . mysql_error());
+		$row = mysql_fetch_array($result);
+		$orderID = $row['orderID'];
 		
 		//Insert info into billing table
-		$sql = "INSERT INTO rrtable_billing (addressID)
-				VALUES ('$bAddressID')";
+		$sql = "INSERT INTO rrtable_billing (addressID, orderID)
+				VALUES ('$bAddressID', '$orderID')";
 		mysql_query($sql, $conn) or die('Billing Input error: ' . mysql_error());
-		
-		//Get bill ID
+				
 		//Insert info into shipping table
-		$sql = "INSERT INTO rrtable_shipping (addressID)
-				VALUES ('$sAddressID')";
+		$sql = "INSERT INTO rrtable_shipping (addressID, orderID)
+				VALUES ('$sAddressID', '$orderID')";
 		mysql_query($sql, $conn) or die('Shipping Input error: ' . mysql_error());
 		
 		//Get shipping ID
-		//INSERT all info into order table
-		//Get order id
+		$sql = "SELECT shipID FROM rrtable_shipping
+				WHERE orderID = '$orderID'";
+		$result = mysql_query($sql, $conn) or die('Shipping ID error: ' . mysql_error());
+		$row = mysql_fetch_array($result);
+		$shipID = $row['shipID'];
 		
-		echo "Total Quantity: " . $totQuan . "<br/>";
-		echo "Subtotal: " . $st . "<br/>";
-		echo "Billing: <br/>";
-		echo $bHouseNumber . " " . $bStreet . "<br/>";
-		echo $bCity . " " . $bState . " " . $bZip . "<br/>";
-		echo "Shipping: <br/>";
-		echo $sHouseNumber . " " . $sStreet . "<br/>";
-		echo $sCity . " " . $sState . " " . $sZip;
+		//Get billing ID
+		$sql = "SELECT billID FROM rrtable_billing
+				WHERE orderID = '$orderID'";
+		$result = mysql_query($sql, $conn) or die('Billing ID error: ' . mysql_error());
+		$row = mysql_fetch_array($result);
+		$billID = $row['billID'];
+		
+		//INSERT all info into order table
+		$sql = "UPDATE rrtable_order SET shipID = '$shipID', billID = '$billID' 
+				WHERE orderID = '$orderID'";
+		mysql_query($sql, $conn) or die('Could not update order info: ' . mysql_error());
+		
 ?>
 <p>Step 1 - Please Enter Billing and Shipping Information <br/>
 Step 2 - Please Verify Accuracy and Make Any Neccessary Changes<br/>
@@ -120,9 +146,61 @@ Step 2 - Please Verify Accuracy and Make Any Neccessary Changes<br/>
 <p>
 Here is a recap of your order:
 </p>
+<?php
+	echo '<p>';
+	echo 'Order Date: ' . $orderDate . '<br/>';
+	echo 'Order Number: ' . $orderID . '<br/>';
+	echo 'Bill To: <br/>';
+	echo $bHouseNumber . " " . $bStreet . "<br/>";
+	echo $bCity . " " . $bState . " " . $bZip . "<br/>";
+	echo '<br/><br/>';
+	echo 'Ship To: <br/>';
+	echo $sHouseNumber . " " . $sStreet . "<br/>";
+	echo $sCity . " " . $sState . " " . $sZip;
+	echo '<br/><br/>__________________________________________<br/><br/>';
+	
+	//get prods
+	//display all items in cart
+		/*$sql = "SELECT prodID, quantity " .
+				"FROM rrTable_transactions " .
+				"WHERE userID=" . $userID . "AND
+				transID=3";
+		$result = mysql_query($sql, $conn) or
+			die("Couldn't retrieve items in cart. " . mysql_error());
+		
+		$xml = simplexml_load_file("assets/xml/productList.xml")
+				or die("Unable to lad XML file");
 
+		echo "<table id='modcart_table'>";
+		echo "<tr><td>Item Image</td><td>Item Name</td><td>Price Each</td><td>Extended Price</td><td>Quantity</td></tr>";
+		$total = 0;
+				
+		while($row = mysql_fetch_array($result)){
+			foreach ($xml->product as $product) {
+				$id = $product->attributes()->id;
+				$name = $product->name;
+				$price = $product->price;
+				$thumbnail = $product->thumbnail;
+				$quan = $product->quantity;
+					
 
-<?php 
+			if($id == $row["prodID"]){
+
+				$total += ($price * $row["quantity"]);
+				echo "<tr>";
+				echo "<td><img src='" . $thumbnail . "' alt='" . $name . "' height='100' width='100'/></td>";
+				echo "<td class='product_name'>" . $name . "</td>";
+				echo "<td><p>$" . $price . "</p></td>";
+				echo "<td><p>$" . $price * $row["quantity"] . "</p></td>";
+				echo $row['quantity'];
+				}
+			}
+		*///}
+	
+	echo 'Your total before shipping is:  $' . $subtotal . '<br/>';
+	echo 'Shipping Costs:  $' . $shipCost . '<br/>';
+	echo 'Your final total is:  $' . $totalCost;
+
 	}else{
 		echo 'Sorry but there was an error:  userID lost.<br/>';
 		echo 'Please <a href="index.php">go back to the main page </a> and try again.';
--- store.sql
+++ store.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 26, 2012 at 07:28 AM
+-- Generation Time: Nov 26, 2012 at 11:45 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -29,20 +29,20 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `rrtable_address` (
   `addressID` int(10) NOT NULL AUTO_INCREMENT,
   `userID` int(10) NOT NULL DEFAULT '0',
-  `houseNum` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `street` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `houseNum` varchar(30) NOT NULL,
+  `street` varchar(50) NOT NULL,
   `zip` int(5) NOT NULL DEFAULT '12345',
   PRIMARY KEY (`addressID`),
   KEY `userID` (`userID`),
   KEY `zip` (`zip`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `rrtable_address`
 --
 
 INSERT INTO `rrtable_address` (`addressID`, `userID`, `houseNum`, `street`, `zip`) VALUES
-(1, 1, '4284', 'Steep Rd', 54501);
+(1, 1, '7691', 'Carver School Rd', 53120);
 
 -- --------------------------------------------------------
 
@@ -52,10 +52,11 @@ INSERT INTO `rrtable_address` (`addressID`, `userID`, `houseNum`, `street`, `zip
 
 CREATE TABLE IF NOT EXISTS `rrtable_billing` (
   `billID` int(10) NOT NULL AUTO_INCREMENT,
+  `addressID` int(10) NOT NULL DEFAULT '0',
   `orderID` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`billID`),
-  KEY `orderID` (`orderID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `addressID` (`addressID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -68,14 +69,7 @@ CREATE TABLE IF NOT EXISTS `rrtable_cart` (
   `prodID` int(10) NOT NULL DEFAULT '0',
   `quantity` int(5) NOT NULL DEFAULT '0',
   PRIMARY KEY (`userID`,`prodID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `rrtable_cart`
---
-
-INSERT INTO `rrtable_cart` (`userID`, `prodID`, `quantity`) VALUES
-(1, 1, 1);
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -88,14 +82,14 @@ CREATE TABLE IF NOT EXISTS `rrtable_citystate` (
   `city` varchar(255) NOT NULL,
   `state` varchar(255) NOT NULL,
   PRIMARY KEY (`zip`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `rrtable_citystate`
 --
 
 INSERT INTO `rrtable_citystate` (`zip`, `city`, `state`) VALUES
-(54501, 'Rhinelander', 'WI');
+(53120, 'East Troy', 'Wisconsin');
 
 -- --------------------------------------------------------
 
@@ -108,14 +102,14 @@ CREATE TABLE IF NOT EXISTS `rrtable_name` (
   `first` varchar(50) NOT NULL,
   `last` varchar(50) NOT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `rrtable_name`
 --
 
 INSERT INTO `rrtable_name` (`userID`, `first`, `last`) VALUES
-(1, 'Jordan', 'Rhode');
+(1, 'Peter', 'Rose');
 
 -- --------------------------------------------------------
 
@@ -127,20 +121,15 @@ CREATE TABLE IF NOT EXISTS `rrtable_order` (
   `orderID` int(10) NOT NULL AUTO_INCREMENT,
   `transID` int(10) NOT NULL DEFAULT '0',
   `orderDate` date NOT NULL DEFAULT '0000-00-00',
-  `sAddressID` int(10) NOT NULL DEFAULT '0',
-  `sZip` int(5) NOT NULL DEFAULT '12345',
-  `bAddressID` int(10) NOT NULL DEFAULT '0',
-  `bZip` int(5) NOT NULL DEFAULT '12345',
+  `shipID` int(10) NOT NULL DEFAULT '0',
+  `billID` int(10) NOT NULL DEFAULT '0',
   `subtotal` float NOT NULL DEFAULT '0',
   `shipCost` float NOT NULL DEFAULT '0',
-  `tax` float NOT NULL DEFAULT '0',
   `total` float NOT NULL DEFAULT '0',
   PRIMARY KEY (`orderID`,`transID`),
-  KEY `sAddressID` (`sAddressID`),
-  KEY `sZip` (`sZip`),
-  KEY `bAddressID` (`bAddressID`),
-  KEY `bZip` (`bZip`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `billID` (`billID`),
+  KEY `shipID` (`shipID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -150,11 +139,11 @@ CREATE TABLE IF NOT EXISTS `rrtable_order` (
 
 CREATE TABLE IF NOT EXISTS `rrtable_shipping` (
   `shipID` int(10) NOT NULL AUTO_INCREMENT,
+  `addressID` int(10) NOT NULL DEFAULT '0',
   `orderID` int(10) NOT NULL DEFAULT '0',
-  `dateShipped` date NOT NULL DEFAULT '0000-00-00',
   PRIMARY KEY (`shipID`),
-  KEY `orderID` (`orderID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `addressID` (`addressID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -164,13 +153,13 @@ CREATE TABLE IF NOT EXISTS `rrtable_shipping` (
 
 CREATE TABLE IF NOT EXISTS `rrtable_transactions` (
   `transID` int(10) NOT NULL DEFAULT '0',
-  `lineNum` int(10) NOT NULL DEFAULT '0',
+  `lineNum` int(10) NOT NULL AUTO_INCREMENT,
   `userID` int(10) NOT NULL DEFAULT '0',
   `prodID` int(10) NOT NULL DEFAULT '0',
   `quantity` int(5) NOT NULL DEFAULT '0',
   PRIMARY KEY (`transID`,`lineNum`,`userID`),
   KEY `userID` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -183,67 +172,18 @@ CREATE TABLE IF NOT EXISTS `rrtable_user` (
   `email` varchar(255) NOT NULL,
   `password` varchar(50) NOT NULL,
   `phone` varchar(15) DEFAULT '000-000-0000',
+  `numTransactions` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`userID`),
   UNIQUE KEY `userID` (`userID`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `rrtable_user`
 --
 
-INSERT INTO `rrtable_user` (`userID`, `email`, `password`, `phone`) VALUES
-(1, 'rhode.jordan@gmail.com', 'R4niAhBJXFSPU', '715-555-1234');
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `rrtable_address`
---
-ALTER TABLE `rrtable_address`
-  ADD CONSTRAINT `rrtable_address_ibfk_2` FOREIGN KEY (`zip`) REFERENCES `rrtable_citystate` (`zip`),
-  ADD CONSTRAINT `rrtable_address_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `rrtable_user` (`userID`);
-
---
--- Constraints for table `rrtable_billing`
---
-ALTER TABLE `rrtable_billing`
-  ADD CONSTRAINT `rrtable_billing_ibfk_1` FOREIGN KEY (`orderID`) REFERENCES `rrtable_order` (`orderID`);
-
---
--- Constraints for table `rrtable_cart`
---
-ALTER TABLE `rrtable_cart`
-  ADD CONSTRAINT `rrtable_cart_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `rrtable_user` (`userID`);
-
---
--- Constraints for table `rrtable_name`
---
-ALTER TABLE `rrtable_name`
-  ADD CONSTRAINT `rrtable_name_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `rrtable_user` (`userID`);
-
---
--- Constraints for table `rrtable_order`
---
-ALTER TABLE `rrtable_order`
-  ADD CONSTRAINT `rrtable_order_ibfk_1` FOREIGN KEY (`sAddressID`) REFERENCES `rrtable_address` (`addressID`),
-  ADD CONSTRAINT `rrtable_order_ibfk_2` FOREIGN KEY (`sZip`) REFERENCES `rrtable_citystate` (`zip`),
-  ADD CONSTRAINT `rrtable_order_ibfk_3` FOREIGN KEY (`bAddressID`) REFERENCES `rrtable_address` (`addressID`),
-  ADD CONSTRAINT `rrtable_order_ibfk_4` FOREIGN KEY (`bZip`) REFERENCES `rrtable_citystate` (`zip`);
-
---
--- Constraints for table `rrtable_shipping`
---
-ALTER TABLE `rrtable_shipping`
-  ADD CONSTRAINT `rrtable_shipping_ibfk_1` FOREIGN KEY (`orderID`) REFERENCES `rrtable_order` (`orderID`);
-
---
--- Constraints for table `rrtable_transactions`
---
-ALTER TABLE `rrtable_transactions`
-  ADD CONSTRAINT `rrtable_transactions_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `rrtable_user` (`userID`);
+INSERT INTO `rrtable_user` (`userID`, `email`, `password`, `phone`, `numTransactions`) VALUES
+(1, 'rosepeter24@gmail.com', 'R4Y.dNd5FfkFs', '262-206-7491', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
