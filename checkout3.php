c388f0739d2ac61ad45c864ab737eaf0d85263ef
Peter Rose <rosepeter24@gmail.com>
2012-11-26 15:39:13 -0600

    What I have for C.O. 3 also

--- checkout3.php
+++ checkout3.php
@@ -4,8 +4,12 @@
 	require_once 'conn.php';
 	
 	if(isset($_SESSION['userID'])){
+
 		//  Set up Variables
 		$userID = $_SESSION['userID'];
+		$bAddressID = $_POST['baddressID'];
+		$sAddressID = $_POST['saddressID'];
+		
 		//get user's transactions
 		$sql = "SELECT numTransactions FROM rrtable_user
 				WHERE userID = '$userID'";
@@ -15,11 +19,13 @@
 		
 		//create transID from it
 		$transID = $usersTransactions + 1;
+		
 		//get information from cart
 		$sql = "SELECT prodID, quantity FROM rrtable_cart
 				WHERE userID = '$userID'";
 		$result = mysql_query($sql, $conn) or die('Cart Information error: ' . mysql_error());
 		while($row = mysql_fetch_array($result)){
+			
 			//Input information into transactions table
 			$prodID = $row['prodID'];
 			$quan = $row['quantity'];
@@ -27,14 +33,62 @@
 					VALUES ('$transID', '$userID', '$prodID', '$quan')";
 			mysql_query($sql, $conn) or die('Record Transaction error: ' . mysql_error());
 		}
+		
 		//Once the data has been recorded, the cart is emptied for that user, and their account should
 		//	show that they have made the transaction
 		$sql = "UPDATE rrtable_user SET numTransactions = '$transID' 
 				WHERE userID = '$userID'";
 		mysql_query($sql, $conn) or die('Could not update user account: ' . mysql_error());
+		
 		//cart
 		$sql = "DELETE FROM rrtable_cart WHERE userID = '$userID'";
 		mysql_query($sql, $conn) or die('Could not delete cart contents:  ' . mysql_error());
+		
+		//Get all the address information for both billing and shipping
+		// Billing
+		$sql = "SELECT houseNum, street, zip FROM rrtable_address
+				WHERE addressID = '$bAddressID'";
+		$result = mysql_query($sql, $conn) or die('Billing Address error: ' . mysql_error());
+		$row = mysql_fetch_array($result);
+		
+		//set hN, st, and zip
+		$bHouseNumber = $row['houseNum'];
+		$bStreet = $row['street'];
+		$bZip = $row['zip'];
+		
+		//get / set city and state
+		$sql = "SELECT city, state FROM rrtable_citystate
+				WHERE zip = '$bZip'";
+		$result = mysql_query($sql, $conn) or die('Billing Address error: ' . mysql_error());
+		$row = mysql_fetch_array($result);
+		$bCity = $row['city'];
+		$bState = $row['state'];
+		
+		// Shipping
+		$sql = "SELECT houseNum, street, zip FROM rrtable_address
+				WHERE addressID = '$sAddressID'";
+		$result = mysql_query($sql, $conn) or die('Shipping Address error: ' . mysql_error());
+		$row = mysql_fetch_array($result);
+		
+		//set hN, st, and zip
+		$sHouseNumber = $row['houseNum'];
+		$sStreet = $row['street'];
+		$sZip = $row['zip'];
+		
+		//get / set city and state
+		$sql = "SELECT city, state FROM rrtable_citystate
+				WHERE zip = '$sZip'";
+		$result = mysql_query($sql, $conn) or die('Shipping Address error: ' . mysql_error());
+		$row = mysql_fetch_array($result);
+		$sCity = $row['city'];
+		$sState = $row['state'];
+		
+		echo "Billing: <br/>";
+		echo $bHouseNumber . " " . $bStreet . "<br/>";
+		echo $bCity . " " . $bState . " " . $bZip . "<br/>";
+		echo "Shipping: <br/>";
+		echo $sHouseNumber . " " . $sStreet . "<br/>";
+		echo $sCity . " " . $sState . " " . $sZip;
 ?>
 <p>Step 1 - Please Enter Billing and Shipping Information <br/>
 Step 2 - Please Verify Accuracy and Make Any Neccessary Changes<br/>
